6d3d5714b9344d63617aadb078f1dd279a9dd632
John Fox <jfox892@gmail.com>
2015-08-02 19:34:06 -0400

    Initial site setup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-$.ready(function(){
-	
-};
+$(document).ready(function(){
+
+	init();
+});
+
+function init(){
+	var windowHeight = $(window).height();
+	$('#landing-section').height(windowHeight); 
+}
