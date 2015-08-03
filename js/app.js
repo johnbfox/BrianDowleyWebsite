829add485be23cf9b38a861004380f719407c3c0
John Fox <jfox892@gmail.com>
2015-08-02 22:32:43 -0400

    Added header, image, landing section, and about template

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-
 	init();
 });
 
 function init(){
 	var windowHeight = $(window).height();
-	$('#landing-section').height(windowHeight); 
+	var toolbarHeight = $('#toolbar').height();
+	$('#landing-section').height(windowHeight-toolbarHeight-30); 
 }
